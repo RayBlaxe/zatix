d4bbc8ed51fbbae1a463645c84a6bd51c3685b40
RayBlaxe <raihan21ti@mahasiswa.pcr.ac.id>
2025-04-25 17:13:40 +0700

    Inisialisasi ZaTix using Next JS

---
+++ src/app/login/ntahlah.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+export default function Ntahlah() {
+    console.log("Ntahlah component rendered");
+
+
+    
+  return (
+    <div onClick={() => alert('a')}>Ntahlah</div>
+  )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Ntahlah from './ntahlah'
+
+export default function page() {
+    
+    
+  return (
+    <div>page <Ntahlah /></div>
+  )
+}
